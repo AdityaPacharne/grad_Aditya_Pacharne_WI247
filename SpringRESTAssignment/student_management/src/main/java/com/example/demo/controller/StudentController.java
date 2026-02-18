d62a45dd96d17523995008516abf7e16f8f3ab29
AdityaPacharne <adityarpacharne@gmail.com>
2026-02-18 14:03:36 +0530

    Spring Rest Assignment

--- SpringRESTAssignment/student_management/src/main/java/com/example/demo/controller/StudentController.java
+++ SpringRESTAssignment/student_management/src/main/java/com/example/demo/controller/StudentController.java
@@ -6,8 +6,10 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -61,6 +63,31 @@ public class StudentController {
 		}
 	}
 	
+    @PutMapping("/students")
+    public String updateStudent(int regNo, @RequestBody Student s) {
+        if(repo.existsById(regNo)) {
+            repo.save(s);
+            return "Student record update";
+        }
+        else {
+            return "Sorry Student Record not found";
+        }
+    }
+    
+    @PatchMapping("/students")
+    public String updateRecord(int regNo, @RequestBody Student s) {
+    	if(!repo.existsById(regNo)) return "Sorry Student Record not found";
+    	Optional<Student> o = repo.findById(regNo);
+    	Student old = o.get();
+    	if(s.getRollNo()    == 0)       s.setRollNo(old.getRollNo());
+    	if(s.getName()      == null)    s.setName(old.getName());
+    	if(s.getStandard()  == 0)       s.setStandard(old.getStandard());
+    	if(s.getSchool()    == null)    s.setSchool(old.getSchool());
+    	if(s.getGender()    == null)    s.setGender(old.getGender());
+    	if(s.getPercentage()== 0)       s.setPercentage(old.getPercentage());
+    	return "Student Record update";
+    }
+	
 	@DeleteMapping("/students")
 	public String deleteOneStudent(int regNo) {
 		if(repo.existsById(regNo)) {
@@ -98,3 +125,4 @@ public class StudentController {
 		return repo.findByGenderAndStandard(gender, standard).size();
 	}
 }
+
